3421dbf17cb990ba72555867c70cd4dc292391c1
Gyeong Hwa Lee <leegaeng@gmail.com>
2026-09-16 01:00:07 +0900

    Expand works into a responsive project gallery

--- content.js
+++ content.js
@@ -81,6 +81,115 @@ window.PAGEON_CONTENT = {
         "https://i.ytimg.com/vi/RhqOpTjo_FI/hqdefault.jpg"
       ],
       "fallbackImage": "assets/og-preview.png"
+    },
+    {
+      "title": "〈부산국제코미디페스티벌〉",
+      "channel": "메인작가",
+      "url": "https://www.youtube.com/watch?v=9c5NxC5R1qk",
+      "remoteThumbs": [
+        "https://i.ytimg.com/vi/9c5NxC5R1qk/maxresdefault.jpg",
+        "https://i.ytimg.com/vi/9c5NxC5R1qk/hqdefault.jpg"
+      ],
+      "fallbackImage": "assets/og-preview.png",
+      "roles": ["개·폐막식 대본", "출연진 케어", "공연 운영"]
+    },
+    {
+      "title": "〈이봉원 40주년 디너쇼〉",
+      "channel": "메인작가",
+      "url": "",
+      "thumb": "assets/works/lee-bong-won-40th-dinner-show.png",
+      "fallbackImage": "assets/works/lee-bong-won-40th-dinner-show.png",
+      "fit": "contain",
+      "roles": ["공연 기획", "대본", "현장케어"]
+    },
+    {
+      "title": "〈망중왕전〉",
+      "channel": "삼성생명 브랜디드 콘텐츠",
+      "url": "https://www.youtube.com/watch?v=EutRVDe1fLM",
+      "remoteThumbs": [
+        "https://i.ytimg.com/vi/EutRVDe1fLM/maxresdefault.jpg",
+        "https://i.ytimg.com/vi/EutRVDe1fLM/hqdefault.jpg"
+      ],
+      "fallbackImage": "assets/og-preview.png",
+      "roles": ["콘텐츠 기획", "대본", "촬영현장케어"]
+    },
+    {
+      "title": "〈주식인바디〉",
+      "channel": "NH투자증권 브랜디드 콘텐츠",
+      "url": "https://www.youtube.com/watch?v=JiW3UDcv9Mg",
+      "remoteThumbs": [
+        "https://i.ytimg.com/vi/JiW3UDcv9Mg/maxresdefault.jpg",
+        "https://i.ytimg.com/vi/JiW3UDcv9Mg/hqdefault.jpg"
+      ],
+      "fallbackImage": "assets/og-preview.png",
+      "roles": ["콘텐츠 기획", "대본", "촬영현장케어"]
+    },
+    {
+      "title": "〈말왕 브이로그〉",
+      "channel": "샥즈 이어폰 브랜디드 콘텐츠",
+      "url": "https://www.youtube.com/watch?v=r1Sg9BW2NZg&t=87s",
+      "remoteThumbs": [
+        "https://i.ytimg.com/vi/r1Sg9BW2NZg/maxresdefault.jpg",
+        "https://i.ytimg.com/vi/r1Sg9BW2NZg/hqdefault.jpg"
+      ],
+      "fallbackImage": "assets/og-preview.png",
+      "roles": ["콘텐츠 기획", "대본", "촬영현장케어"]
+    },
+    {
+      "title": "〈IM파서블〉",
+      "channel": "대구은행 브랜디드 콘텐츠",
+      "url": "https://www.youtube.com/watch?v=aTFvhFukYRw",
+      "remoteThumbs": [
+        "https://i.ytimg.com/vi/aTFvhFukYRw/maxresdefault.jpg",
+        "https://i.ytimg.com/vi/aTFvhFukYRw/hqdefault.jpg"
+      ],
+      "fallbackImage": "assets/og-preview.png",
+      "roles": ["콘텐츠 기획", "대본", "촬영현장케어"]
+    },
+    {
+      "title": "〈인사적시점〉",
+      "channel": "에이치닷 브랜디드 콘텐츠",
+      "url": "https://www.youtube.com/watch?v=QNdp1Dp1bYo&list=PLRgkPJ1mdOAgzgVSTMyZLz4wIyMsE_HLV&index=2",
+      "remoteThumbs": [
+        "https://i.ytimg.com/vi/QNdp1Dp1bYo/maxresdefault.jpg",
+        "https://i.ytimg.com/vi/QNdp1Dp1bYo/hqdefault.jpg"
+      ],
+      "fallbackImage": "assets/og-preview.png",
+      "roles": ["콘텐츠 기획", "대본", "촬영현장케어"]
+    },
+    {
+      "title": "〈충청남도 적극행정 공무원 홍보영상〉",
+      "channel": "홍보영상",
+      "url": "https://www.youtube.com/watch?v=rT6SoPB43BA",
+      "remoteThumbs": [
+        "https://i.ytimg.com/vi/rT6SoPB43BA/maxresdefault.jpg",
+        "https://i.ytimg.com/vi/rT6SoPB43BA/hqdefault.jpg"
+      ],
+      "fallbackImage": "assets/og-preview.png",
+      "roles": ["콘텐츠 기획", "대본"]
+    },
+    {
+      "title": "〈암모르니아〉",
+      "channel": "급식게임즈 마인크래프트 장기 상황극",
+      "url": "https://www.youtube.com/watch?v=2WU5wuJ8pa4&list=PLCX5rIDml19I490kUAWbWnznnI6e5PXZ4",
+      "remoteThumbs": [
+        "https://i.ytimg.com/vi/2WU5wuJ8pa4/maxresdefault.jpg",
+        "https://i.ytimg.com/vi/2WU5wuJ8pa4/hqdefault.jpg"
+      ],
+      "fallbackImage": "assets/og-preview.png",
+      "roles": ["콘텐츠 기획", "대본"]
+    },
+    {
+      "title": "〈골드만 비뇨기과〉",
+      "channel": "쇼츠 드라마",
+      "url": "https://www.youtube.com/watch?v=Ph8uzxBXFCw&t=1s",
+      "remoteThumbs": [
+        "https://i.ytimg.com/vi/Ph8uzxBXFCw/maxresdefault.jpg",
+        "https://i.ytimg.com/vi/Ph8uzxBXFCw/hqdefault.jpg"
+      ],
+      "fallbackImage": "assets/og-preview.png",
+      "roles": ["콘텐츠 기획", "대본", "촬영", "편집"]
     }
   ]
 };
+
